803c6a87f8b39189d6c72288c55a91682fce15b9
SeungyeonWoo <seungyeon3287@gmail.com>
2026-05-26 23:27:34 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/eodigatji/eodigatjiserver/EodigatjiServerApplication.java
@@ -0,0 +1,13 @@
+package eodigatji.eodigatjiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodigatjiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EodigatjiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eodigatji/eodigatjiserver/EodigatjiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eodigatji.eodigatjiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodigatjiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
